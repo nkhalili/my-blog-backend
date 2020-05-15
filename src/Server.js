665463e65ab5c49d71b33d6c16a142e3b01c9507
Navid <nkhalili@live.com>
2020-05-15 12:14:36 -0400

    Return status code for upvotes

--- src/Server.js
+++ src/Server.js
@@ -1,15 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-// to create our app
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0,
+  },
+  'learn-node': {
+    upvotes: 0,
+  },
+  'my-thoughts-on-resumes': {
+    upvotes: 0,
+  },
+};
+
 const app = express();
 
 app.use(bodyParser.json());
 
-// to create an endpoint for our app (get request)
-app.get('/hello', (req, res) => res.send('hello'));
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
 
-// to add listener
+  articlesInfo[articleName].upvotes += 1;
+  res
+    .status(200)
+    .send(
+      `Article ${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
+});
+
 app.listen(8000, () => console.log('Listening on port 8000'));
